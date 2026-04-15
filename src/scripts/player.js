3933f181449c6360adee0af3b76fc32629f52eda
Jee <jeecoding09@gmail.com>
2026-04-15 12:25:43 +0800

    chore: Updated gameboard and type prop

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,7 +1,7 @@
 export default class Player {
    constructor(gameboard, name, isPlayer = false) {
-      this.gameboard = gameboard.board
+      this.gameboard = gameboard
       this.name = name
-      this.type = isPlayer ? 'player' : 'computer'
+      this.type = isPlayer ? 'human' : 'computer'
    }
 }
