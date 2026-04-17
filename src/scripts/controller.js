47b7195f46c2e95f116de9ba1eb08222b9047fa5
Jee <jeecoding09@gmail.com>
2026-04-17 22:35:46 +0800

    feat: expand Controller class with initialization and management methods

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,5 +1,44 @@
+import Gameboard from "./gameboard"
+import Ship from "./ship"
+import Player from "./player"
+
 export default class Controller {
+   /* 
+      Functions:
+      This class is instantiate when the player pressed start
+      * init -> Initializes the players and their respective gameboard
+      * manageShips -> Make the 2 players set their ship 
+      * manageRound -> Start ng battle. 
+         * This function loops like this. ( Shoot, check ships, switch ) Then repeat  
+      * notifyPlayer -> A methods that choose a random text (arr) when the players shot 
+         * Seperate arrays for hit shot and missed shot  
+      *   
+   */
+
    constructor() {
-      
+      this.human = null
+      this.computer = null
+      this.currentPlayer = null
+      this.winner = null
    }  
+
+   #manageShips() {
+      
+   }
+
+   #switchPlayer() {
+      
+   }
+
+   init() {
+      const GBS = [new Gameboard(), new Gameboard()]
+      this.human = new Player(GBS[0], 'Jee', true)
+      this.computer = new Player(GBS[1], 'Tyler')
+      this.#manageShips()
+   }
+
+   start() {
+      
+   }
+
 }
